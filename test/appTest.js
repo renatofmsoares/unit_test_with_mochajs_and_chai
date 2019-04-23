aebc8a2d6d4873190489477f1d024a20133907c7
Renato Soares <renatof.m.soares@gmail.com>
2019-04-22 22:53:21 -0300

    getting all functions from app using an unique require call

--- test/appTest.js
+++ test/appTest.js
@@ -1,26 +1,26 @@
 const assert = require('chai').assert;
-const sayHello = require('../app').sayHello;
-const addNumbers = require('../app').addNumbers;
-// const app = require('../app');
+// const sayHello = require('../app').sayHello;
+// const addNumbers = require('../app').addNumbers;
+const app = require('../app');
 
 describe('App', function(){
     it('sayHello should return hello', function(){
-        let result = sayHello();
+        let result = app.sayHello();
         assert.equal(result,'hello');
     });
 
     it('sayHello should return type string', function(){
-        let result = sayHello();
+        let result = app.sayHello();
         assert.typeOf(result, 'string');
     });
 
     it('addNumbers should be above 5', function(){
-        let result = addNumbers(5,0);
+        let result = app.addNumbers(5,1);
         assert.isAbove(result, 5);    
     });
 
     it('addNumbers should return type number', function(){
-        let result = addNumbers(5,1);
+        let result = app.addNumbers(5,1);
         assert.typeOf(result, 'number');    
     })
 });
